c0d56b1dbf6478a958d1cf2f93f5c00e0a44cd87
gabrielpires-1 <111147078+gabrielpires-1@users.noreply.github.com>
2022-11-09 17:13:40 -0300

    Add files via upload

---
+++ morseCode.ino
@@ -0,0 +1,103 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+int timeStart = 0;
+int buttonMorse = 13;
+int buttonEnd = 12;
+int buttonMorseState = 0;
+int buttonEndState = 0;
+int timeButtonPressed = 0;
+int buttonStartEnd = 11;
+int buttonStartEndState= 0;
+int cont = 0;
+int timeButtonNotPressed_Start = 0;
+int timeButtonNotPressed = 0;
+int buzzer = 10;
+int lcdPos = 0;
+
+LiquidCrystal_I2C lcd(0x3F,16,2);
+
+// morse character
+String character = "";
+// list of letters and numbers
+String lettersAndNumbers[36]={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"};
+// respective morse code for each letter and number
+String morseCode[36]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..",
+"-----",".----","..---","...--","....-",".....","-....","--...","---..","----."};
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonMorse, INPUT);
+  pinMode(buttonEnd, INPUT);
+  pinMode(buttonStartEnd, INPUT);
+  pinMode(buzzer, OUTPUT);
+
+  lcd.init(); // Serve para iniciar a comunicação com o display já conectado
+  lcd.backlight(); // Serve para ligar a luz do display
+  lcd.clear(); // Serve para limpar a tela do display
+  lcd.setCursor(0,0);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if (lcdPos%32==0) {
+    lcd.clear();
+  }
+  buttonStartEndState = digitalRead(buttonStartEnd);
+  if(buttonStartEndState){
+    cont++;
+    delay(500);
+  }
+  if(cont%2==1){
+      timeButtonNotPressed = millis();
+      if (timeButtonNotPressed % 3000 == 0){
+      Serial.print(" ");
+      lcd.print(" ");
+      lcdPos ++;
+      delay(300);
+      }
+  // put your main code here, to run repeatedly:
+  buttonMorseState = digitalRead(buttonMorse);
+  buttonEndState = digitalRead(buttonEnd);
+  // timeStart restart before press the button
+  timeStart = millis();
+
+// while the button is pressed the time is count
+  while (buttonMorseState){
+    buttonMorseState = digitalRead(buttonMorse);
+    tone(buzzer, 800);
+  }
+  noTone(buzzer);
+// finalize the time and store in timeButtonPressed
+  timeButtonPressed = millis() - timeStart;
+// if the button is pressed for less than 200 miliseconds and more than 5 miliseconds, so the character is "."
+  if (timeButtonPressed < 200 and timeButtonPressed > 5){
+    character = character + ".";
+    timeButtonNotPressed = timeButtonNotPressed - timeButtonNotPressed;
+  }
+  // if the button is pressed for more than 200 seconds, so the character is "-"
+  if (timeButtonPressed > 200) {
+    character = character + "-";
+    timeButtonNotPressed = timeButtonNotPressed - timeButtonNotPressed;
+  }
+  // if the button that ends the character is pressed
+  if (buttonEndState){
+    // compare the list of characters with the list of morse
+     for(int i = 0;i < 36; i++){
+       // if the character made is equal to the respective morse code, print the character that have the same index
+       if(morseCode[i]==character){
+         Serial.print(lettersAndNumbers[i]);
+         lcd.print(lettersAndNumbers[i]);
+         lcdPos ++;
+         if(lcdPos%16==0){
+          lcd.setCursor(0,1);
+         }
+       }
+     }
+     // character is restarted
+    character = "";
+    delay(500);
+  }
+  }
+}
