4aaf0df19861df50872daa0c4e1e7f08c744ea58
gabrielpires-1 <111147078+gabrielpires-1@users.noreply.github.com>
2022-11-10 14:41:17 -0300

    Comentários

--- morseCode.ino
+++ morseCode.ino
@@ -32,9 +32,9 @@ void setup() {
   pinMode(buttonStartEnd, INPUT);
   pinMode(buzzer, OUTPUT);
 
-  lcd.init(); // Serve para iniciar a comunicação com o display já conectado
-  lcd.backlight(); // Serve para ligar a luz do display
-  lcd.clear(); // Serve para limpar a tela do display
+  lcd.init(); // Inicialize communication with the display
+  lcd.backlight(); // Turn the display lights on
+  lcd.clear(); // Clean the display
   lcd.setCursor(0,0);
   Serial.begin(9600);
 
@@ -42,27 +42,30 @@ void setup() {
 
 void loop() {
   if (lcdPos%32==0) {
-    lcd.clear();
+    // Everytime all the 32 positions of the display are being used, clean it up
+    lcd.clear(); 
   }
   buttonStartEndState = digitalRead(buttonStartEnd);
+  // Everytime the button thats start and pauses the program is pressed, the program increases 'cont' in one
   if(buttonStartEndState){
     cont++;
     delay(500);
   }
+  // If 'cont' is even, the program starts, if it's odd, the program pauses
   if(cont%2==1){
+      // When the program is running, the time that the button of the morse code is not pressed is count
       timeButtonNotPressed = millis();
+      // If the button is not pressed for 3 seconds, a space is done
       if (timeButtonNotPressed % 3000 == 0){
       Serial.print(" ");
       lcd.print(" ");
       lcdPos ++;
       delay(300);
       }
-  // put your main code here, to run repeatedly:
   buttonMorseState = digitalRead(buttonMorse);
   buttonEndState = digitalRead(buttonEnd);
   // timeStart restart before press the button
   timeStart = millis();
-
 // while the button is pressed the time is count
   while (buttonMorseState){
     buttonMorseState = digitalRead(buttonMorse);
@@ -74,11 +77,13 @@ void loop() {
 // if the button is pressed for less than 200 miliseconds and more than 5 miliseconds, so the character is "."
   if (timeButtonPressed < 200 and timeButtonPressed > 5){
     character = character + ".";
+    // the time that the button is not pressed is turned to 0
     timeButtonNotPressed = timeButtonNotPressed - timeButtonNotPressed;
   }
   // if the button is pressed for more than 200 seconds, so the character is "-"
   if (timeButtonPressed > 200) {
     character = character + "-";
+    // the time that the button is not pressed is turned to 0
     timeButtonNotPressed = timeButtonNotPressed - timeButtonNotPressed;
   }
   // if the button that ends the character is pressed
@@ -89,7 +94,9 @@ void loop() {
        if(morseCode[i]==character){
          Serial.print(lettersAndNumbers[i]);
          lcd.print(lettersAndNumbers[i]);
+         // Position of the display's cursor is increased in one
          lcdPos ++;
+         // If the position is in the end of the first line, everytime lcdPos is divisible by 16, the cursor go to the second line, position 0,1
          if(lcdPos%16==0){
           lcd.setCursor(0,1);
          }
